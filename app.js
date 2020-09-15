a67b2df2732e2e909182db5dcbc4de7bcb3831b0
gavrilciunterei <gavriltaller@gmail.com>
2020-09-15 23:01:26 +0300

    Configurar cabeceras y cors

--- app.js
+++ app.js
@@ -10,7 +10,17 @@ var project_routes = require('./routes/project');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); //convierte los dartos a json
 
-//cors
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 //rutas
 app.use('/', project_routes);
